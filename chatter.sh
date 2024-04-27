1ff80443387b2685c962e3f0989393057c33fa3b
OctoLinkYT <138608589+OctoLinkYT@users.noreply.github.com>
2024-04-27 18:35:01 -0400

    Create chatter.sh

---
+++ chatter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# DON'T FORGET TO PUT THIS IN ROOT!
+/usr/local/bin/ngrok http --domain=morally-elegant-pig.ngrok-free.app 8080 &
+
+# Start PHP server
+php -S localhost:8080 -t chatter
